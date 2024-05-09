07a2f3e956549327e9433343bc68a15bcf92746a
emalineg <mgayhart0@ivc.edu>
2024-05-08 18:09:58 -0700

    update readme

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -9,7 +9,7 @@ const HomeCard: React.FC<HomeCardProps> = ({ title, imageUrl }) => {
   return (
     <div className="mx-auto flex h-auto w-full max-w-md overflow-hidden rounded-lg border-4 border-red-800 bg-white shadow-lg md:w-full md:max-w-2xl">
       <div
-        className="h-48 w-1/2 bg-cover" // Set a fixed height for uniformity
+        className="h-48 w-1/2 bg-cover"
         style={{ backgroundImage: `url(${imageUrl})` }}
       ></div>
       <div className="w-1/2 p-4 md:p-8">
